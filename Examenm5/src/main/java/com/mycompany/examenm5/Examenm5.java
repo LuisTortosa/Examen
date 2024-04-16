5aff9dd2fb65c3686e58ce2f1b6cf921ecf662e0
Luis <Luis@DESKTOP-SN0LDC4>
2024-04-16 19:45:29 +0200

    Push 1

---
+++ Examenm5/src/main/java/com/mycompany/examenm5/Examenm5.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.examenm5;
+
+/**
+ *
+ * @author Luis
+ */
+public class Examenm5 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
